3641183103e21d83e9ffdcede3614ec0733f6139
JRIX-DESKTOP-2\jrixg <jrixgeromo@gmail.com>
2024-10-27 09:26:54 +0800

    updates

--- x.php
+++ x.php
@@ -2,25 +2,17 @@ function transformDateFormat(array $dates): array {
     $result = [];
     
     foreach ($dates as $date) {
-        // Check for format YYYY/MM/DD
+        // Match YYYY/MM/DD
         if (preg_match('/^(\d{4})\/(\d{2})\/(\d{2})$/', $date, $matches)) {
-            if (checkdate((int)$matches[2], (int)$matches[3], (int)$matches[1])) {
-                $formattedDate = $matches[1] . $matches[2] . $matches[3];
-                $result[] = $formattedDate;
-            }
+            $result[] = $matches[1] . $matches[2] . $matches[3];
         }
-        // Check for format MM-DD-YYYY
+        // Match MM-DD-YYYY
         elseif (preg_match('/^(\d{2})-(\d{2})-(\d{4})$/', $date, $matches)) {
-            if (checkdate((int)$matches[1], (int)$matches[2], (int)$matches[3])) {
-                $formattedDate = $matches[3] . $matches[1] . $matches[2];
-                $result[] = $formattedDate;
-            }
+            $result[] = $matches[3] . $matches[1] . $matches[2];
         }
-        // Check for format YYYYMMDD without delimiters
-        elseif (preg_match('/^(\d{4})(\d{2})(\d{2})$/', $date, $matches)) {
-            if (checkdate((int)$matches[2], (int)$matches[3], (int)$matches[1])) {
-                $result[] = $date;
-            }
+        // Match YYYYMMDD (already in the correct format)
+        elseif (preg_match('/^(\d{4})(\d{2})(\d{2})$/', $date)) {
+            $result[] = $date;
         }
     }
     
