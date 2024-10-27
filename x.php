5310426ec641a58684cd3f9ed20c1d4c8ce72161
JRIX-DESKTOP-2\jrixg <jrixgeromo@gmail.com>
2024-10-27 09:00:09 +0800

    updates

---
+++ x.php
@@ -0,0 +1,28 @@
+function transformDateFormat(array $dates): array {
+    $result = [];
+    
+    foreach ($dates as $date) {
+        // Check format YYYY/MM/DD
+        if (preg_match('/^(\d{4})\/(\d{2})\/(\d{2})$/', $date, $matches)) {
+            $formattedDate = $matches[1] . $matches[2] . $matches[3];
+            $result[] = $formattedDate;
+        }
+        // Check format MM-DD-YYYY
+        elseif (preg_match('/^(\d{2})-(\d{2})-(\d{4})$/', $date, $matches)) {
+            $formattedDate = $matches[3] . $matches[1] . $matches[2];
+            $result[] = $formattedDate;
+        }
+        // Check format YYYY DDD MM (assuming it's YYYYMMDD without delimiters)
+        elseif (preg_match('/^(\d{8})$/', $date)) {
+            $result[] = $date;
+        }
+    }
+    
+    return $result;
+}
+
+// Test case
+$dates = transformDateFormat([
+    "2010/02/20", "2 016p 19p 12", "11-18-2012", "2018 12 24", "20130720"
+]);
+print_r($dates);
