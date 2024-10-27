58373e3b9c9cace6f0374ccac0eb516a2f9667b6
JRIX-DESKTOP-2\jrixg <jrixgeromo@gmail.com>
2024-10-27 09:49:23 +0800

    updates

--- x.php
+++ x.php
@@ -1,25 +1,7 @@
--- Suggested testing environment:
--- https://www.db-fiddle.com/ with MySQL version set to 8
-
--- Example case create statement:
-CREATE TABLE students (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    score INTEGER NOT NULL,
-    class INTEGER NOT NULL    
-);
-
-INSERT INTO students(id, name, score, class) VALUES(1, 'Mark', 894, 7);
-INSERT INTO students(id, name, score, class) VALUES(2, 'Bill', 894, 7);
-INSERT INTO students(id, name, score, class) VALUES(3, 'Maria', 678, 8);
-INSERT INTO students(id, name, score, class) VALUES(4, 'David', 733, 9);
-INSERT INTO students(id, name, score, class) VALUES(5, 'John', 899, 9);
-INSERT INTO students(id, name, score, class) VALUES(6, 'Rob', 802, 9);
-
--- Expected output (rows in any order):
--- name    score    class
--- -------------------------
--- Mark    894      7       
--- Bill    894      7             
--- Maria   678      8
--- John    899      9
+SELECT s.name, s.score, s.class
+FROM students s
+JOIN (
+    SELECT class, MAX(score) AS max_score
+    FROM students
+    GROUP BY class
+) AS max_scores ON s.class = max_scores.class AND s.score = max_scores.max_score;
