8456336e09677dd63b52eb43bc310892ed8e4731
jrixgeromo <jrixgeromo@gmail.com>
2024-10-28 15:40:18 +0800

    updates

--- x.php
+++ x.php
@@ -1,7 +1,38 @@
-SELECT s.name, s.score, s.class
-FROM students s
-JOIN (
-    SELECT class, MAX(score) AS max_score
-    FROM students
-    GROUP BY class
-) AS max_scores ON s.class = max_scores.class AND s.score = max_scores.max_score;
+<?php
+
+class ConstructionGame 
+{
+    public function __construct(int $length, int $width) 
+    {
+        // Write the code that goes here
+    }
+
+    public function addCubes(array $cubes) : void
+    {
+        // Write the code that goes here
+    }
+
+    public function getHeight() : int  
+    {
+        // Write the code that goes here
+        return -1;
+    }
+}
+
+$game = new ConstructionGame(2, 2);
+
+$game->addCubes([
+    [true, true],
+    [false, false]
+]);
+$game->addCubes([
+    [true, true],
+    [false, true]
+]);
+echo $game->getHeight() . "\n"; // should print 2
+
+$game->addCubes([
+    [false, false],
+    [true, true]
+]);
+echo $game->getHeight() . "\n"; // should print 1
