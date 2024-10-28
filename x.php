9197a408e582d96d761ce618d5636d2c66bf0a63
JRIX-DESKTOP-2\jrixg <jrixgeromo@gmail.com>
2024-10-28 15:54:29 +0800

    update

--- x.php
+++ x.php
@@ -1,24 +1,40 @@
-<?php
-
 class ConstructionGame 
 {
+    private $heights;
+    private $length;
+    private $width;
+
     public function __construct(int $length, int $width) 
     {
-        // Write the code that goes here
+        $this->length = $length;
+        $this->width = $width;
+        $this->heights = array_fill(0, $length, array_fill(0, $width, 0));
     }
 
     public function addCubes(array $cubes) : void
     {
-        // Write the code that goes here
+        for ($i = 0; $i < $this->length; $i++) {
+            for ($j = 0; $j < $this->width; $j++) {
+                if ($cubes[$i][$j] === true) {
+                    $this->heights[$i][$j]++;
+                }
+            }
+        }
     }
 
     public function getHeight() : int  
     {
-        // Write the code that goes here
-        return -1;
+        $maxHeight = 0;
+        foreach ($this->heights as $row) {
+            foreach ($row as $height) {
+                $maxHeight = max($maxHeight, $height);
+            }
+        }
+        return $maxHeight;
     }
 }
 
+// Test the implementation
 $game = new ConstructionGame(2, 2);
 
 $game->addCubes([
@@ -35,4 +51,4 @@ $game->addCubes([
     [false, false],
     [true, true]
 ]);
-echo $game->getHeight() . "\n"; // should print 1
+echo $game->getHeight() . "\n"; // should print 2
